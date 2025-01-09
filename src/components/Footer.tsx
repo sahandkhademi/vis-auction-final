09a80833e88230b155d2788ba2b7fc54bf77aba1
sahandkhademi <sahandkhademi@icloud.com>
2025-01-09 10:45:42 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
               <h3 className="text-xl font-serif text-gray-900">VIS Auction</h3>
             </div>
             <p className="text-sm text-gray-600">
-              Connecting Young Artists with Patreons.
+              Connecting young artists with patreons.
             </p>
           </div>
           
